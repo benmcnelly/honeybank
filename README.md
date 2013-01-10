8576c765b0e61ab6d417221f50278fd3057fbe3e
Ben McNelly <ben@picreate.com>
2013-01-10 01:56:50 -0600

    Compiled Sencha and organised files

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 honeybank
 =========
+Changelog:
+
+Generated sencha app source files. Turns out you need to download and install the Sencha Cmd program too. After that I set up the git repo in the /www/ folder I can server files out of (localhost) and moved the project files in there. For giggles I dropped a copy of the whole Sencha SDK in there so I could play with the example apps. I guess Sencha needs a webserver to work before compiling. What a slop nugget.
+
+Long long story short, I can hit localhost/honeybank/ and it serves up to folders. One has the static app (just html/css) and the other has the full blown generated sencha app. I guess once the app does what we want, to compile we are going to have to move the files from the /honeybank-sencha-app/ folder to the root of /honeybank/ and set up the config files for phonegap/cordova/whateveritscalled.
+
+=========
 
 A honey pot app that looks like your bank account. If anyone starts snooping around in the app, we email you and let you know when it happened.
---
+++ honeybank-sencha-app/app/view/Main.js
@@ -0,0 +1,50 @@
+Ext.define('honeybank.view.Main', {
+    extend: 'Ext.tab.Panel',
+    xtype: 'main',
+    requires: [
+        'Ext.TitleBar',
+        'Ext.Video'
+    ],
+    config: {
+        tabBarPosition: 'bottom',
+
+        items: [
+            {
+                title: 'First Run',
+                iconCls: 'home',
+
+                styleHtmlContent: true,
+                scrollable: true,
+
+                items: {
+                    docked: 'top',
+                    xtype: 'titlebar',
+                    title: 'Honey Bank'
+                },
+
+                html: [
+                    "<h3>Dear Sencha...</h3>",
+                    "Your getting started documents are full of s***. Anywhoo, this is the contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
+                    "and refresh to change what's rendered here."
+                ].join("")
+            },
+            {
+                title: 'How it works',
+                iconCls: 'action',
+
+                items: [
+                    {
+                        docked: 'top',
+                        xtype: 'titlebar',
+                        title: 'How it works / test video'
+                    },
+                    {
+                        xtype: 'video',
+                        url: 'http://av.vimeo.com/26826/820/21598416.mp4?token=1357803919_c6a8d060e511ed05b48ba3c74e490abc',
+                        posterUrl: 'http://icons-search.com/img/yellowicon/Flat_Linux.zip/Flat_for_Linux-Movies-42-Movies_256x256.png-256x256.png'
+                    }
+                ]
+            }
+        ]
+    }
+});
